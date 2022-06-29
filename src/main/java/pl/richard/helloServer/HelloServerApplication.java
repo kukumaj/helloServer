ecb1a299760ab685732d2b6d736f75f977081ac3
Ryszard <52924153+kukumaj@users.noreply.github.com>
2022-06-29 23:48:12 +0200

    Initial commit

---
+++ src/main/java/pl/richard/helloServer/HelloServerApplication.java
@@ -0,0 +1,28 @@
+package pl.richard.helloServer;
+
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.ipc.netty.http.server.HttpServer;
+
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+public class HelloServerApplication {
+
+    public static void main(String[] args) {
+        RouterFunction route = route(GET("/"),
+                request -> ServerResponse.ok().body(fromValue("Hello")));
+//        HttpHandler httpHandler = RouterFunctions.toHttpHandler(route);
+//        HttpServer server = HttpServer.create("localhost", 8080);
+//        server.startAndAwait(new ReactorHttpHandlerAdapter(httpHandler));
+        HttpHandler handler = RouterFunctions.toHttpHandler(route);
+        ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(handler);
+        HttpServer server = HttpServer.create("localhost", 8080);
+        ser9ver.startAndAwait(adapter);
+    }
+
+}
---
+++ src/test/java/pl/richard/helloServer/HelloServerApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.richard.helloServer;
+
+import org.junit.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+class HelloServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
